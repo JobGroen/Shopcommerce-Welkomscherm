9b60cc2b95295f301bbe3f0bf260806db849b3fe
JobGroen <meneergroen@outlook.com>
2018-04-19 15:56:26 +0200

    .

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ function getInput($data) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title</title>
+    <title>Welkom bij Shopcommerce</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
