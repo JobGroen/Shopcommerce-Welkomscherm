b1a65d2490647e88d155b13026d434c2c5626e9c
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-04-23 16:06:09 +0200

    temperatuur 2.0
    temperatuur refresht automatich en een celsius teken staat er bij :+1:

---
+++ js/temprefresh.js
@@ -0,0 +1,17 @@
+var temp = document.getElementById("temp");
+
+function temprefresh() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                temp.innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "php/temperatuur.php", true);
+    xhttp.send();
+}
+temprefresh();
+setInterval(function(){ temprefresh(); }, 30000);
+
+
