9edbc9bdb182573199d0dd2f13263e41d858d68e
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-04-24 15:33:34 +0200

    weather icons
    er zit nog wel een fout in de code maar hier zijn de weather_icons alvast

--- php/temperatuur.php
+++ php/temperatuur.php
@@ -7,10 +7,10 @@ curl_setopt($verify, CURLOPT_URL, $api_uri);
 curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
 
-$connection = curl_exec($verify);
-
+echo $connection = curl_exec($verify);
+echo '<br><br>';
 $connection = json_decode($connection, true);
-
+var_dump($connection);
+echo '<br><br>';
 $celcius = ($connection["main"]["temp"] - 273.15 ) * 1.000000;
-echo round($celcius, 0) . "°c";
-?>
+echo '<img src="images/weather_icons/'. $connection["weather"][""]["icon"] .'.png" alt="Weather_icon" height="42" width="42">' .round($celcius, 0) . '°c';
