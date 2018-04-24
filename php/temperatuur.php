655438e82495ea10e9b4c5bc2d2758edacef23c6
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-04-24 16:07:26 +0200

    image_temperatuur gefixed
    het plaatje bij de temperatuur staat er in :smile:

--- php/temperatuur.php
+++ php/temperatuur.php
@@ -7,10 +7,9 @@ curl_setopt($verify, CURLOPT_URL, $api_uri);
 curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
 
-echo $connection = curl_exec($verify);
-echo '<br><br>';
+$connection = curl_exec($verify);
+
 $connection = json_decode($connection, true);
-var_dump($connection);
-echo '<br><br>';
+
 $celcius = ($connection["main"]["temp"] - 273.15 ) * 1.000000;
-echo '<img src="images/weather_icons/'. $connection["weather"][""]["icon"] .'.png" alt="Weather_icon" height="42" width="42">' .round($celcius, 0) . '°c';
+echo '<img style="float:left; width: 77px;" src="../images/weather_icons/'. $connection["weather"][0]["icon"] .'.png" alt="Weather_icon" height="42" width="42">' .round($celcius, 0) . '°c';
