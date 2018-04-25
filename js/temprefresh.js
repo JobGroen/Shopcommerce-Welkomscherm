69d61bf7fcf3948a32c3ea0d31fb9145d57f24fa
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-04-25 11:44:16 +0200

    .

--- js/temprefresh.js
+++ js/temprefresh.js
@@ -5,7 +5,7 @@ function temprefresh() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
-                temp.innerHTML = xhttp.responseText;
+            temp.innerHTML = xhttp.responseText;
         }
     };
     xhttp.open("GET", "php/temperatuur.php", true);
